6a54a10458576cbd90d14d79df2fbfb7a5384cbd
WhiteApfel <github@pfel.ru>
2019-11-24 20:45:27 +0700

    Output improvement. Improved brute force.

--- hindex.py
+++ hindex.py
@@ -1,10 +1,17 @@
 def h_index(publications):
     publications = sorted(publications)
     N = len(publications)
-    for h in range(0, N):
-        if publications[-h:][0] >= h and publications[:N-h][-1] <= h:
-            return h
-    return 1
+    if N > 1:
+        maxrange = min([N, max(publications)]) + 1
+        for h in range(1, maxrange)[::-1]:
+            if publications[-h:][0] >= h >= publications[:N - h][-1]:
+                return h
+        return 1
+    else:
+        return 1
+
 
 if __name__ == "__main__":
-    print(h_index(list(map(int, input().split())))) #"Enter the number of citations for each publication, separated by spaces.\n\n>>> "
+    print("Enter the number of citations for each publication, separated by spaces.\n\n")
+    while True:
+        print(f'H-index: \033[93m{h_index(list(map(int, input(">>> ").split())))}\033[0m\n')
